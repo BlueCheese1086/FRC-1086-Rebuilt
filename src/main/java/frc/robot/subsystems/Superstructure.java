ea483715c52902cf6d509300dc85c741219b9482
macsk <mac.skaanes@gmail.com>
2026-01-25 21:28:15 -0500

    added a basic setup for state machine will discuss later

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -4,25 +4,38 @@
 
 package frc.robot.subsystems;
 
+import java.util.HashMap;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.drive.Drive;
 
 public class Superstructure extends SubsystemBase {
-  public enum SuperstructureState { // Ideas
-    Intaking,
-    Travelling,
-    Targetting,
-    Shooting,
-    Passing,
-    Climbing,
-    Disabled,
-    Idle
-  }
-  /** Creates a new Superstructure. */
-  public Superstructure(final Drive drive) {
-    
+  
+  public enum State { // Ideas
+    intake,
+    travel,
+    target,
+    shoot,
+    pass,
+    climb,
+    disabled,
+    idle
   }
 
+  @AutoLogOutput( key = "Superstructure/Current State")
+  private State currentState = State.disabled;
+  private HashMap<State,Trigger> stateMap = new HashMap<State,Trigger>();
+  /** Creates a new Superstructure. */
+  public Superstructure(final Drive drive) {
+    for (State state : State.values()) {
+      stateMap.put(state, new Trigger(() -> {return currentState == state;}));
+    }
+  }
+
+  // Logging Only!
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
