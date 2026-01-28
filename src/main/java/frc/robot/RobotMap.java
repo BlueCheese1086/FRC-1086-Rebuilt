be6fd6192b19dafe0511aadba216ee63bb4c7124
macsk <mac.skaanes@gmail.com>
2026-01-28 15:39:33 -0500

    added robot map

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,5 +4,34 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.CANBus;
+
+import frc.robot.generated.TunerConstants;
+
 /** Add your docs here. */
-public class RobotMap {}
+public class RobotMap {
+    public static final CANBus rioBus = new CANBus("Alternate");
+    public static final CANBus driveBus = TunerConstants.kCANBus;
+    public static final CANBus systemBus = new CANBus("Canivore2");
+
+    // TODO: Replace all these with the actual ids
+    public static class Intake {
+        public static final int pivot = 0;
+        public static final int roller = 0;
+    }
+
+    public static final int feeder = 0;
+
+    public static class Shooter {
+        public static final int left = 0;
+        public static final int middle = 0;
+        public static final int right = 0;
+    }
+
+    public static class Hood {
+        public static final int left = 0;
+        public static final int right = 0;
+    }
+
+    public static final int climber = 0;
+}
