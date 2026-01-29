0860a39d59fae679a229b653567df00ba9e4be73
macsk <mac.skaanes@gmail.com>
2026-01-29 17:29:08 -0500

    build constants doesn't like me

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 42;
-  public static final String GIT_SHA = "f34ca19b29186e7b0aaf9d83a5027886524aa7ea";
-  public static final String GIT_DATE = "2026-01-29 11:48:15 EST";
+  public static final int GIT_REVISION = 44;
+  public static final String GIT_SHA = "75207c792d19161f5444f3abae34c701b21feaf3";
+  public static final String GIT_DATE = "2026-01-29 17:27:39 GMT-05:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-29 12:29:26 EST";
-  public static final long BUILD_UNIX_TIME = 1769707766365L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-29 17:28:29 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1769725709214L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
