883c417fff5561d1c15ac7957b8b6400529edb81
macsk <mac.skaanes@gmail.com>
2026-01-30 09:34:47 -0500

    made spotless exclude the wpilib to avoid it accidentally breaking the projects.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 55;
-  public static final String GIT_SHA = "41f78f992abfb763e3182ffcf586425e8a9dd223";
-  public static final String GIT_DATE = "2026-01-30 00:14:00 GMT-05:00";
+  public static final int GIT_REVISION = 56;
+  public static final String GIT_SHA = "333ad82b6c2d261ad70af51926928d05ed273420";
+  public static final String GIT_DATE = "2026-01-30 09:22:04 GMT-05:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-30 09:21:24 GMT-05:00";
-  public static final long BUILD_UNIX_TIME = 1769782884747L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-30 09:33:54 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1769783634861L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
