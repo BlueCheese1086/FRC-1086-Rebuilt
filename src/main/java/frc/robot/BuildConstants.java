01b699fb7bcb524e9cb2631de83004f6d93befa4
MSTC-Industries <mstc.industries.official@gmail.com>
2026-02-12 17:14:20 -0500

    added reverse starting poses for choreo

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 127;
-  public static final String GIT_SHA = "47b2a1345ad89fb351d76554c38a7fc77ddd24e1";
-  public static final String GIT_DATE = "2026-02-10 20:30:10 EST";
+  public static final int GIT_REVISION = 128;
+  public static final String GIT_SHA = "c07fb8ab14de2ec122210b560f4daeeb61ec88a4";
+  public static final String GIT_DATE = "2026-02-10 22:51:21 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-10 22:50:15 EST";
-  public static final long BUILD_UNIX_TIME = 1770781815787L;
+  public static final String BUILD_DATE = "2026-02-11 09:41:07 EST";
+  public static final long BUILD_UNIX_TIME = 1770820867037L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
