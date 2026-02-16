3ee5393be789a36e51af9197574208e052284998
antigro09 <anthonyj.cavero@gmail.com>
2026-02-15 21:56:50 -0500

    small fixes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,19 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 113;
-  public static final String GIT_SHA = "3da5021f4c4ad7942d79972d71dd5c4edbc23e71";
-  public static final String GIT_DATE = "2026-02-07 14:42:35 EST";
-  public static final int GIT_REVISION = 152;
-  public static final String GIT_SHA = "2f3fe1cb598798f91f0761f5eb49b41b05a3b46e";
-  public static final String GIT_DATE = "2026-02-14 15:37:45 EST";
+  public static final int GIT_REVISION = 157;
+  public static final String GIT_SHA = "fb186a5590df3d5845137d9423410ea677b591a5";
+  public static final String GIT_DATE = "2026-02-15 21:18:33 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-07 15:43:07 EST";
-  public static final long BUILD_UNIX_TIME = 1770496987643L;
+  public static final String BUILD_DATE = "2026-02-15 21:24:53 EST";
+  public static final long BUILD_UNIX_TIME = 1771208693891L;
   public static final int DIRTY = 1;
-  public static final String BUILD_DATE = "2026-02-15 15:13:29 EST";
-  public static final long BUILD_UNIX_TIME = 1771186409929L;
-  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
