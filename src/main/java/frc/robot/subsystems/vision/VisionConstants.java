40c169d8a42b67a127278e71303bd9c29252153d
Blasty25 <sohamshah210@gmail.com>
2026-02-28 14:57:08 -0500

    pusdahfkljadsk;lfj

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -37,13 +37,13 @@ public class VisionConstants {
   public static Transform3d robotToLeftCam =
       new Transform3d(
           -Units.inchesToMeters(12.0),
-          -Units.inchesToMeters(12.0),
+          Units.inchesToMeters(12.0),
           Units.inchesToMeters(6.0),
           new Rotation3d(0.0, -Units.degreesToRadians(150.0), Units.degreesToRadians(0)));
   public static Transform3d robotToRightCam =
       new Transform3d(
           -Units.inchesToMeters(12.0),
-          Units.inchesToMeters(12.0),
+          -Units.inchesToMeters(12.0),
           Units.inchesToMeters(6.0),
           new Rotation3d(0.0, -Units.degreesToRadians(150.0), Units.degreesToRadians(0)));
 
