add006d7d13e30b66e64f1224ea11afcc806daba
antigro09 <anthonyj.cavero@gmail.com>
2026-03-08 19:56:58 -0400

    pls

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 372;
-  public static final String GIT_SHA = "d03759e6af8184b8f04e92c41582b7e9086486e5";
-  public static final String GIT_DATE = "2026-03-08 14:54:13 EDT";
+  public static final int GIT_REVISION = 373;
+  public static final String GIT_SHA = "0b14015a6a3d5f2bdf6f9296f80e6ed71e91a1e9";
+  public static final String GIT_DATE = "2026-03-08 17:03:09 EDT";
   public static final String GIT_BRANCH = "New-Branch-1";
-  public static final String BUILD_DATE = "2026-03-08 16:53:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1773003225866L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-08 19:31:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1773012702540L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
