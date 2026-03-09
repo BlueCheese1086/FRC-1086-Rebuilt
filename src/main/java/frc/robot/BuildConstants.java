76c845a2702e9b40dd6ae41c0f6eff5fa43b92e9
Blasty25 <sohamshah210@gmail.com>
2026-03-08 22:33:35 -0400

    added autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 376;
-  public static final String GIT_SHA = "b1620722edd16ffd495ff2f32e36398ac8009110";
-  public static final String GIT_DATE = "2026-03-08 21:24:58 EDT";
+  public static final int GIT_REVISION = 377;
+  public static final String GIT_SHA = "5983a4d2c67dfd9712870fc5b5805730ad5fa96f";
+  public static final String GIT_DATE = "2026-03-08 21:54:52 EDT";
   public static final String GIT_BRANCH = "Manual-Drive";
-  public static final String BUILD_DATE = "2026-03-08 21:41:48 EDT";
-  public static final long BUILD_UNIX_TIME = 1773020508934L;
+  public static final String BUILD_DATE = "2026-03-08 22:24:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1773023074530L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
