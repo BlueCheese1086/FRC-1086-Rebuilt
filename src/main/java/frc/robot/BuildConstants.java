2b598f7c0b9dda1ea47b44cf8ad64cb8100f4acc
FRC 1086 Competition Drive Laptop <team@bluecheeserobotics.org>
2026-03-12 15:28:21 -0800

    build constants updated

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 404;
-  public static final String GIT_SHA = "26c00333577da39a60e0326b4ebb25c4e1bf53c3";
-  public static final String GIT_DATE = "2026-03-12 18:19:34 EDT";
-  public static final String GIT_BRANCH = "Manual-Drive";
-  public static final String BUILD_DATE = "2026-03-12 18:37:54 EDT";
-  public static final long BUILD_UNIX_TIME = 1773355074467L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 405;
+  public static final String GIT_SHA = "e2c0ed41b400e9bc395fbb8ea905df025ed0f1d6";
+  public static final String GIT_DATE = "2026-03-12 18:59:52 EDT";
+  public static final String GIT_BRANCH = "Regency-3-12-2026";
+  public static final String BUILD_DATE = "2026-03-12 19:26:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1773357979380L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
