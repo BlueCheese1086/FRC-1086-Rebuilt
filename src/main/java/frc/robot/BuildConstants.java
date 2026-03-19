622271015999aeb109bc81bd8e16483a929ba42a
MSTC-Industries <mstc.industries.official@gmail.com>
2026-03-18 21:34:15 -0400

    fixed pathes again

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 414;
-  public static final String GIT_SHA = "61f2ca0da9564f08491c2a937577472775cddc2d";
-  public static final String GIT_DATE = "2026-03-17 18:05:26 EDT";
-  public static final String GIT_BRANCH = "Week2";
-  public static final String BUILD_DATE = "2026-03-17 20:37:38 EDT";
-  public static final long BUILD_UNIX_TIME = 1773794258386L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 425;
+  public static final String GIT_SHA = "bed529a405f8df881b542e238099e2fe881ded77";
+  public static final String GIT_DATE = "2026-03-18 21:30:05 EDT";
+  public static final String GIT_BRANCH = "AutonomousImprovements";
+  public static final String BUILD_DATE = "2026-03-18 21:30:41 EDT";
+  public static final long BUILD_UNIX_TIME = 1773883841238L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
