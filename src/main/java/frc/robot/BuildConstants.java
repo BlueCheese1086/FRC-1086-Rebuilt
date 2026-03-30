afc7b09b67879112e114e3b7568b89d08388ef30
antigro09 <anthonyj.cavero@gmail.com>
2026-03-30 14:59:44 -0400

    lowk why didnt it update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 442;
-  public static final String GIT_SHA = "85ef08315572e39d5cf9fb2e9c7845fbda2989e4";
-  public static final String GIT_DATE = "2026-03-30 13:24:24 EDT";
+  public static final int GIT_REVISION = 443;
+  public static final String GIT_SHA = "c78cc1a9a012c53b1855fbe23c677398fc6c8a20";
+  public static final String GIT_DATE = "2026-03-30 14:51:27 EDT";
   public static final String GIT_BRANCH = "Week4";
-  public static final String BUILD_DATE = "2026-03-30 14:25:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1774895114784L;
+  public static final String BUILD_DATE = "2026-03-30 14:51:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1774896707035L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
