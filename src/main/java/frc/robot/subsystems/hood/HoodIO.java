bd88043a3a0055765a24cfc9c50f54aefdbaccf5
Blasty25 <sohamshah210@gmail.com>
2026-04-02 18:59:28 -0400

    encoder

--- src/main/java/frc/robot/subsystems/hood/HoodIO.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIO.java
@@ -19,6 +19,9 @@ public interface HoodIO {
     public double leftPosition;
     public boolean rightAtSetpoint;
     public double rightPosition;
+
+    public double hoodAngle;
+    public double absoulteAngle;
   }
 
   public default void updateInputs(HoodInputs inputs) {}
