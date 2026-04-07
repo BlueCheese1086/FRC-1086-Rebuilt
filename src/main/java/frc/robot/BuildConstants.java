251a67d5a8643fef5d42a63191a04d78dcf02400
macsk <mac.skaanes@gmail.com>
2026-04-07 18:36:14 -0400

    built

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 491;
-  public static final String GIT_SHA = "3847e7f49f258f636b54f834e25ff81149eb01fc";
-  public static final String GIT_DATE = "2026-04-07 18:23:46 EDT";
+  public static final int GIT_REVISION = 493;
+  public static final String GIT_SHA = "452a8c7a5fdf4f22da42dee661f9503d227df9da";
+  public static final String GIT_DATE = "2026-04-07 18:30:06 GMT-04:00";
   public static final String GIT_BRANCH = "Champs";
-  public static final String BUILD_DATE = "2026-04-07 18:27:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1775600835658L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-04-07 18:30:26 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1775601026125L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
