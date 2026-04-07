9a099f2f8d1522b5f2461067bb65a6b705e1d4c0
Anthony Cavero <anthonyj.cavero@gmail.com>
2026-04-07 16:34:49 -0400

    Fix import statement for RumbleType

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilinj2.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj2.GenericHID.RumbleType;
 import frc.robot.autonomous.Autos;
 import frc.robot.autonomous.AutosManager;
 import frc.robot.commands.DriveCommands;
