9762ca26ce0b927a4398badcd18fd870fbc5ddd7
MSTC-Industries <mstc.industries.official@gmail.com>
2026-04-07 18:30:33 -0400

    apparently you cannot have nested folders in /choreo

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1086-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 486;
-  public static final String GIT_SHA = "d1cd5b7af7966731b9ac8b3b23383e6b20ebbcfe";
-  public static final String GIT_DATE = "2026-04-07 18:13:02 GMT-04:00";
+  public static final int GIT_REVISION = 491;
+  public static final String GIT_SHA = "3847e7f49f258f636b54f834e25ff81149eb01fc";
+  public static final String GIT_DATE = "2026-04-07 18:23:46 EDT";
   public static final String GIT_BRANCH = "Champs";
-  public static final String BUILD_DATE = "2026-04-07 18:14:24 GMT-04:00";
-  public static final long BUILD_UNIX_TIME = 1775600064895L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-04-07 18:27:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1775600835658L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
